2e44d401610d10fb7ca7de2f983d2af1a8a79461
Drelf2018 <drelf2018@outlook.com>
2023-10-17 02:58:33 +0800

    feat: 新增微博适配器

--- submitter/__init__.py
+++ submitter/__init__.py
@@ -1,17 +1,20 @@
+from . import adapters
+from .adapters import *
 from .client import Client, Submitter, delta, run_forever
-from .data import Attachment, Blogger, Job, Post, User, parse
+from .data import Attachment, Attachments, Blogger, Job, Post, User
 from .network import HEADERS, Api, ApiException, Session
 
 __all__ = [
+    "adapters",
     "Api",
     "ApiException",
     "Attachment",
+    "Attachments",
     "Blogger",
     "Client",
     "delta",
     "HEADERS",
     "Job",
-    "parse",
     "Post",
     "run_forever",
     "Session",
