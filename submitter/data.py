eeea57746af305deb32bb01b678d7219f2e022ed
Drelf2018 <drelf2018@outlook.com>
2023-10-17 02:57:54 +0800

    fix: 优化数据结构

--- submitter/data.py
+++ submitter/data.py
@@ -24,15 +24,38 @@ class Attachment:
         return json.dumps(self.data, ensure_ascii=False)
 
 
+def Attachments(*urls: str) -> List[Attachment]:
+    return [Attachment(u) for u in urls if u is not None and u != ""]
+
+
+@dataclass
+class Online:
+    server: int
+    users: Dict[str, int]
+
+
 @dataclass
 class Job:
-    id: int
     patten: str
     method: str
     url: str
     data: Dict[str, str] = field(default_factory=dict)
     headers: Dict[str, str] = field(default_factory=dict)
     cookies: Dict[str, str] = field(default_factory=dict)
+    id: int = 0
+
+    @property
+    def json(self) -> dict:
+        """
+        返回字典格式经 json 处理后对象
+        """
+        dic = {}
+        for k, v in self.__dict__.items():
+            if k in ["data", "headers", "cookies"]:
+                dic[k] = json.dumps(v)
+            else:
+                dic[k] = str(v)
+        return dic
 
 
 @dataclass
@@ -80,11 +103,15 @@ class Post:
     time: str
     text: str
     source: str
-    blogger: Blogger    
-    repost: "Post"
+    blogger: Blogger
     comments: List["Post"]
     attachments: List[Attachment]
+    repost: "Post" = None
     submitter: User = None
+
+    def set_repost(self, repost: "Post") -> "Post":
+        self.repost = repost
+        return self
 
     @property
     def date(self) -> str:
@@ -128,32 +155,35 @@ class Post:
             elif k == "attachments":
                 dic[k] = [str(v) for v in self.attachments]
             elif k == "repost":
-                dic[k] = "null" if self.repost is None else str(self.repost)
+                dic[k] = "null" if self.repost is None else self.repost.string()
             elif k == "comments":
-                dic[k] = [str(v) for v in self.comments]
+                dic[k] = [v.string() for v in self.comments]
             else:
                 if v is None:
                     continue
                 dic[k] = str(v)
         return dic
 
-    def __str__(self) -> str:
+    def string(self) -> str:
         return json.dumps(self.data, ensure_ascii=False)
-
-
-def parse(post: dict) -> Post:
-    """
-    递归解析
-    """
-    if post is None or len(post) == 0:
-        return None
-    post.update(post.pop("blogger"))
-    return Post(
-        face        = Attachment(**post.pop("face")),
-        pendant     = Attachment(**post.pop("pendant")),
-        attachments = [Attachment(**v) for v in post.pop("attachments")],
-        repost      = parse(post.pop("repost")),
-        comments    = [parse(v) for v in post.pop("comments")],
-        submitter   = User(**post.pop("submitter")),
-        **post
-    )
+    
+    @staticmethod
+    def parse(post: dict) -> "Post":
+        """
+        递归解析
+        """
+        if post is None or len(post) == 0:
+            return None
+        user: dict = post.pop("blogger")
+        return Post(
+            blogger     = Blogger(
+                face    = Attachment(**user.pop("face")),
+                pendant = Attachment(**user.pop("pendant")),
+                **user,
+            ),
+            comments    = [Post.parse(v) for v in post.pop("comments")],
+            attachments = [Attachment(**v) for v in post.pop("attachments")],
+            repost      = Post.parse(post.pop("repost")),
+            submitter   = User(**post.pop("submitter")),
+            **post,
+        )
